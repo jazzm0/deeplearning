3f5acb3ecce99ad1356b19920044777bd69838a4
Tibor Tarnai <tibor.tarnai@sap.com>
2020-03-26 11:47:27 +0100

    improve solution to 83% accuracy

--- bonus_week2.py
+++ bonus_week2.py
@@ -25,7 +25,7 @@ test_label_vectorized = keras.utils.to_categorical(test_label, total_classes)
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),  # input
-    keras.layers.Dense(128, activation='sigmoid'),  # hidden
+    keras.layers.Dense(128, activation='relu'),  # hidden
     keras.layers.Dense(total_classes, activation='sigmoid')  # output
 ])
 
@@ -40,4 +40,4 @@ model.fit(train_images, train_label_vectorized, epochs=30)
 eval_loss, eval_accuracy = model.evaluate(test_images, test_label_vectorized)
 print(eval_loss, eval_accuracy)
 
-Submission('a70a2614a4468a25eb66a1113c846e31', '2', model).submit()
+# Submission('a70a2614a4468a25eb66a1113c846e31', '2', model).submit()
